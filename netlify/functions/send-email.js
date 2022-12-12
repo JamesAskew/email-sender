a89a6de5616daef84b0a903b3a7ee412630164ca
James Askew <james.askew@hudl.com>
2022-12-12 00:21:01 +0000

    removed some headers

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -66,9 +66,6 @@ exports.handler = async (event) => {
     body: responseBody,
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Methods": "POST",
       "Access-Control-Allow-Headers": "Content-Type",
     },
   };
