0f541bb9f38e3faf33c835e411e21b96cacb8ec1
James Askew <james.askew@hudl.com>
2023-01-24 23:40:00 +0000

    console.log payload

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -59,6 +59,7 @@ function formatSubject(jsonPayload) {
 
 exports.handler = async (event) => {
   const jsonPayload = JSON.parse(event.body);
+  console.log("jsonPayload: ", jsonPayload);
 
   if (jsonPayload.key != EMAIL_KEY) {
     return {
